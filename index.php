091634126a56912bbbb34a0cdc5727bcf383499c
Jovibe Code <nahjonah00@gmail.com>
2025-12-30 01:40:51 +0100

    Revert "fix: login redirect"
    This reverts commit 33bb60a872d19e60d170dc4d6bd38b2bde78f77a.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 session_start();
 
 require_once __DIR__ . '/config/database.php';
-// require_once __DIR__ . '/src/functions.php';
+require_once __DIR__ . '/src/functions.php';
 
 // Process any pending profits
 triggerProcessPendingProfits();
