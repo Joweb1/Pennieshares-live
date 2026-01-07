7e0bf3868fddb56f0ddd919035059f3cd648f9b7
Jovibe Code <nahjonah00@gmail.com>
2026-01-08 00:04:45 +0100

    fix:slow loading

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/src/functions.php';
 
 // Process any pending profits
-triggerProcessPendingProfits();
+// triggerProcessPendingProfits();
 
 $request_uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 
